88a0d70a65854f72fdc1822d720fcd75c56645ca
shuoyue <1162288059@qq.com>
2022-09-28 14:16:02 +0800

    mergeSort modified

--- SortAlgorithm/05_mergeSort.cpp
+++ SortAlgorithm/05_mergeSort.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <algorithm>
 using namespace std;;
 
 template<typename T>
@@ -15,6 +16,10 @@ public:
         if (data == nullptr) return;
         delete[] data;
         data = nullptr;
+    }
+    Field_(const Field_& other):Field_{other.n} {
+        for(int i = 0; i < n; i++)
+            data[i] = other(i);
     }
     Field_& operator=(double s) {
         for (int i = 0; i < n; i++)
@@ -36,20 +41,10 @@ public:
 using FieldI = Field_<int>;
 using FieldD = Field_<double>;
 
-class Merge {
+template<typename T>
+class sortAlgorithm {
 public:
-    Merge(int n) : data(n), gen(std::random_device()()), rnd(0, 1000) {}
-    void dataGeneration() {
-        for (int i = 0; i < data.n; i++)
-            data[i] = rnd(gen);
-    }
-
-    void dataShow() {
-        for (int i = 0; i < data.n; i++)
-            cout << data(i) << ((i + 1) % 15 ? ' ' : '\n');
-    }
-
-    void mergeSort(FieldI& data, int front, int end) {
+    void mergeSort(Field_<T>& data, int front, int end) {
         if(front >= end)
             return;
         int mid = front + (end - front) / 2;
@@ -58,7 +53,7 @@ public:
         merge(data, front, mid, end);
     }
 
-    void mergeSort1() {
+    void mergeSort1(Field_<T>& data) {
         // int size = 1, low = 0, mid = 0, high = 0;
         // while(size < data.n) {
         //     low = 0;
@@ -83,7 +78,7 @@ public:
         }
     }
 
-    void merge(FieldI& data, int front, int mid, int end) {
+    void merge(Field_<T>& data, int front, int mid, int end) {
         FieldI tmp(end - front + 1);
         int i = front, j = mid + 1, k = 0;
         while(i <= mid && j <= end)
@@ -101,39 +96,107 @@ public:
         data[ix] = data[jx];
         data[jx] = tmp;
     }
-
-    FieldI data;
-protected:
-    std::mt19937 gen;
-    std::uniform_int_distribution<int> rnd;
 };
 
+namespace Functions {
+    template<typename T>
+    Field_<T>& dataGeneration(Field_<T>& data) {
+        std::mt19937 gen(std::random_device{} ());
+        std::uniform_int_distribution<T> rnd(-1000, 1000);
+        for (int i = 0; i < data.n; i++)
+                data[i] = rnd(gen);
+        return data;
+    }
+
+    template<typename T>
+    void dataShow(Field_<T>& data) {
+        for (int i = 0; i < data.n; i++)
+            cout << data(i) << ((i + 1) % 15 ? ' ' : '\n');
+    }
+
+    template<typename T>
+    bool isEqual(Field_<T>& arr1, Field_<T>& arr2) {
+        bool equal = true;
+        for(int i = 0; i < arr1.n; i++)
+            if(arr1[i] != arr2[i]) {
+                equal = false;
+                break;
+            }
+        return equal;
+    }
+
+    template<typename T>
+    void testCorrect(sortAlgorithm<T>& solver, int num) {
+        Field_<T> data(num);
+
+        int testTime = 30000;
+        bool succed = true;
+        for(int it = 0; it < testTime; it++) {
+            FieldI arr1 = dataGeneration(data);
+            FieldI arr2 = arr1, arr3 = arr1;
+            // before sort
+            // cout << "Before Sort" << endl;
+            // cout << "【arr1】" << endl;
+            // dataShow(arr1);
+            // cout << endl << "【arr2】" << endl;
+            // dataShow(arr2);
+            // cout << endl << "【arr3】" << endl;
+            // dataShow(arr3);
+            
+            // sort
+            solver.mergeSort(arr1, 0, arr1.n - 1);
+            // solver.mergeSort1(arr1);
+            sort(arr2.data, arr2.data + arr2.n);
+            if(!isEqual(arr1, arr2)) {
+                succed = false;
+                break;
+            }
+            
+            // after sort
+            // cout << endl << endl << "After Sort" << endl;
+            // cout << "【arr1】" << endl;
+            // dataShow(arr1);
+            // cout << endl << "【arr2】" << endl;
+            // dataShow(arr2);
+            // cout << endl << "【arr3】" << endl;
+            // dataShow(arr3);
+        }
+        cout << endl << (succed ? "Sort algorithm correct!!" : "So sorry, you failed!!") << endl;
+    }
+};
+
+/*====================================================================*/
 int main(int argc, char* argv[]) {
     int num = 20;
     if(argc > 1)
         num = atoi(argv[1]);
-    Merge merge(num);
-    merge.dataGeneration();
+    
+    sortAlgorithm<int> solver;
+    Functions::testCorrect<int>(solver, num);
+    
+    FieldI data(num);
+    data = Functions::dataGeneration<int>(data);
     cout << "Before sort:" << endl;
-    merge.dataShow();
+    Functions::dataShow(data);
     
     auto timeS = chrono::high_resolution_clock::now();
-    merge.mergeSort(merge.data, 0, merge.data.n - 1);
+    solver.mergeSort(data, 0, data.n - 1);
     auto timeE = chrono::high_resolution_clock::now();
     chrono::duration<double> timeDelta = timeE - timeS;
     cout << endl << "After sort:" << timeDelta.count() <<" s" <<endl;
-    merge.dataShow();
+    Functions::dataShow(data);
     cout << endl;
 
     cout << "===============================" <<endl;
-    merge.dataGeneration();
+    data = Functions::dataGeneration<int>(data);
     cout << endl << "Before sort:" << endl;
-    merge.dataShow();
+    Functions::dataShow(data);
     timeS = chrono::high_resolution_clock::now();
-    merge.mergeSort1();
+    solver.mergeSort1(data);
     timeE = chrono::high_resolution_clock::now();
     timeDelta = timeE - timeS;
     cout << endl << "After sort1:" << timeDelta.count() <<" s" <<endl;
-    merge.dataShow();
+    Functions::dataShow(data);
+    cout << endl;
     return 0;
 }
