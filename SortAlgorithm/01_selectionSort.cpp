7adc644f789fbaa5eee771c860ca18695685b66b
shuoyue <1162288059@qq.com>
2022-09-28 13:41:15 +0800

    selectionSort modified

--- SortAlgorithm/01_selectionSort.cpp
+++ SortAlgorithm/01_selectionSort.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <sys/select.h>
+#include <algorithm>
 using namespace std;;
 
 template<typename T>
@@ -15,6 +17,10 @@ public:
         if (data == nullptr) return;
         delete[] data;
         data = nullptr;
+    }
+    Field_(const Field_& other):Field_{other.n} {
+        for(int i = 0; i < n; i++)
+            data[i] = other(i);
     }
     Field_& operator=(double s) {
         for (int i = 0; i < n; i++)
@@ -36,26 +42,20 @@ public:
 using FieldI = Field_<int>;
 using FieldD = Field_<double>;
 
-class Selection {
+template<typename T>
+class sortAlgorithm {
 public:
-    Selection(int n) : data(n), gen(std::random_device()()), rnd(0, 1000) {}
-    void dataGeneration() {
-        for (int i = 0; i < data.n; i++)
-            data[i] = rnd(gen);
-    }
-    void dataShow() {
-        for (int i = 0; i < data.n; i++)
-            cout << data(i) << ((i + 1) % 15 ? ' ' : '\n');
-    }
-    void selectionSort() {
+    void selectionSort(Field_<T>& data) {
+        if(data.n < 2) return;
         for (int i = 0; i < data.n - 1; i++) {
             int minIx = i;
             for (int j = i + 1; j < data.n; j++)
                 minIx = data[minIx] > data[j] ? j : minIx;
-            dataExchange(data, i, minIx);
+            swap(data, i, minIx);
         }
     }
-    void selectionSort2() {
+    void selectionSort2(Field_<T>& data) {
+        if(data.n < 2) return;
         for(int left = 0, right = data.n - 1; left < right; left++, right--) {
             int minIx = left;
             int maxIx = right;
@@ -63,48 +63,116 @@ public:
                 minIx = data[minIx] > data[i] ? i : minIx;
                 maxIx = data[maxIx] < data[i] ? i : maxIx;
             }
-            dataExchange(data, left, minIx);
+            swap(data, left, minIx);
             if(left == maxIx)
                 maxIx = minIx;
-            dataExchange(data, right, maxIx);
+            swap(data, right, maxIx);
         }
     }
-    void dataExchange(FieldI& data, int& ix, int& jx) {
-        int tmp = data[ix];
+    void swap(Field_<T>& data, int& ix, int& jx) {
+        T tmp = data[ix];
         data[ix] = data[jx];
         data[jx] = tmp;
     }
-    FieldI data;
-protected:
-    std::mt19937 gen;
-    std::uniform_int_distribution<int> rnd;
 };
 
+namespace Functions {
+    template<typename T>
+    Field_<T>& dataGeneration(Field_<T>& data) {
+        std::mt19937 gen(std::random_device{} ());
+        std::uniform_int_distribution<T> rnd(-1000, 1000);
+        for (int i = 0; i < data.n; i++)
+                data[i] = rnd(gen);
+        return data;
+    }
+
+    template<typename T>
+    void dataShow(Field_<T>& data) {
+        for (int i = 0; i < data.n; i++)
+            cout << data(i) << ((i + 1) % 15 ? ' ' : '\n');
+    }
+
+    template<typename T>
+    bool isEqual(Field_<T>& arr1, Field_<T>& arr2) {
+        bool equal = true;
+        for(int i = 0; i < arr1.n; i++)
+            if(arr1[i] != arr2[i]) {
+                equal = false;
+                break;
+            }
+        return equal;
+    }
+
+    template<typename T>
+    void testCorrect(sortAlgorithm<T>& solver, int num) {
+        Field_<T> data(num);
+
+        int testTime = 30000;
+        bool succed = true;
+        for(int it = 0; it < testTime; it++) {
+            FieldI arr1 = dataGeneration(data);
+            FieldI arr2 = arr1, arr3 = arr1;
+            // before sort
+            // cout << "Before Sort" << endl;
+            // cout << "【arr1】" << endl;
+            // dataShow(arr1);
+            // cout << endl << "【arr2】" << endl;
+            // dataShow(arr2);
+            // cout << endl << "【arr3】" << endl;
+            // dataShow(arr3);
+            
+            // sort
+            solver.selectionSort(arr1);
+            sort(arr2.data, arr2.data + arr2.n);
+            if(!isEqual(arr1, arr2)) {
+                succed = false;
+                break;
+            }
+            
+            // after sort
+            // cout << endl << endl << "After Sort" << endl;
+            // cout << "【arr1】" << endl;
+            // dataShow(arr1);
+            // cout << endl << "【arr2】" << endl;
+            // dataShow(arr2);
+            // cout << endl << "【arr3】" << endl;
+            // dataShow(arr3);
+        }
+        cout << endl << (succed ? "Sort algorithm correct!!" : "So sorry, you failed!!") << endl;
+    }
+};
+
+
+//================================================================================//
 int main(int argc, char* argv[]) {
     int num = 20;
     if(argc > 1)
         num = atoi(argv[1]);
-    Selection s(num);
-    s.dataGeneration();
+    
+    sortAlgorithm<int> solver;
+    Functions::testCorrect<int>(solver, num);
 
+    FieldI data(num);
+    data = Functions::dataGeneration<int>(data);
     cout << "Before sort:" << endl;
-    s.dataShow();
+    Functions::dataShow(data);
     
     auto timeS = chrono::high_resolution_clock::now();
-    s.selectionSort();
+    solver.selectionSort(data);
     auto timeE = chrono::high_resolution_clock::now();
     chrono::duration<double> timeDelta = timeE - timeS;
     cout << endl << endl << "After sort:" << endl;
     cout << "Selection sort time consumption: " << timeDelta.count() <<" s" <<endl;
-    s.dataShow();
+    Functions::dataShow(data);
     
-    s.dataGeneration();
+    cout << "===============================" <<endl;
+    data = Functions::dataGeneration(data);
     timeS = chrono::high_resolution_clock::now();
-    s.selectionSort2();
+    solver.selectionSort2(data);
     timeE = chrono::high_resolution_clock::now();
     timeDelta = timeE - timeS;
     cout << endl << endl << "After sort2:" << endl;
     cout << "Selection sort2 time consumption: " << timeDelta.count() <<" s" <<endl;
-    s.dataShow();
+    Functions::dataShow(data);
     return 0;
 }
--- SortAlgorithm/02_bubbleSort.cpp
+++ SortAlgorithm/02_bubbleSort.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <algorithm>
 using namespace std;;
 
 template<typename T>
@@ -15,6 +16,10 @@ public:
         if (data == nullptr) return;
         delete[] data;
         data = nullptr;
+    }
+    Field_(const Field_& other):Field_{other.n} {
+        for(int i = 0; i < n; i++)
+            data[i] = other(i);
     }
     Field_& operator=(double s) {
         for (int i = 0; i < n; i++)
@@ -36,33 +41,23 @@ public:
 using FieldI = Field_<int>;
 using FieldD = Field_<double>;
 
-class Bubble {
+template<typename T>
+class sortAlgorithm {
 public:
-    Bubble(int n) : data(n), gen(std::random_device()()), rnd(0, 1000) {}
-    void dataGeneration() {
-        for (int i = 0; i < data.n; i++)
-            data[i] = rnd(gen);
-    }
-
-    void dataShow() {
-        for (int i = 0; i < data.n; i++)
-            cout << data(i) << ((i + 1) % 15 ? ' ' : '\n');
-    }
-
-    void bubbleSort() {
+    void bubbleSort(Field_<T>& data) {
         for (int i = 0; i < data.n - 1; i++) {
             for (int j = 1; j < data.n - i; j++)
                 if(data[j-1] > data[j])
-                    dataExchange(data, j, j - 1);
+                    swap(data, j, j - 1);
         }
     }
 
-    void bubbleSort1() {
+    void bubbleSort1(Field_<T>& data) {
         for(int i = 0; i < data.n - 1; i++) {
             bool exchanged = false;
             for(int j = 0; j < data.n - 1 - i; j++) {
                 if(data[j] > data[j+1]) {
-                    dataExchange(data, j, j + 1);
+                    swap(data, j, j + 1);
                     exchanged = true;
                 }
             }
@@ -71,14 +66,14 @@ public:
         }
     }
 
-    void bubbleSort2() {
+    void bubbleSort2(Field_<T>& data) {
         int lastLoc = data.n;
         int tmpPos = 0;
         for(int i = 0; i < data.n - 1; i++) {
             bool exchanged = false;
             for(int j = 0; j < lastLoc - 1; j++) {
                 if(data[j] > data[j+1]) {
-                    dataExchange(data, j, j + 1);
+                    swap(data, j, j + 1);
                     tmpPos = j + 1;
                     exchanged = true;
                 }
@@ -89,14 +84,14 @@ public:
         }
     }
 
-    void bubbleSort3() {
+    void bubbleSort3(Field_<T>& data) {
         int leftLasChangeIx = 0;
         int rightLastChangIx = data.n - 1;
         for (int i = 0; i < data.n / 2; i++) {
             int tmpIx = -1;
             for (int j = leftLasChangeIx; j < rightLastChangIx; j++) {
                 if (data[j] > data[j + 1]) {
-                    dataExchange(data, j, j + 1);
+                    swap(data, j, j + 1);
                     tmpIx = j + 1;
                 }
             }
@@ -107,7 +102,7 @@ public:
             tmpIx = -1;
             for(int k = rightLastChangIx; k >leftLasChangeIx; k--) {
                 if(data[k] < data[k-1]){
-                    dataExchange(data, k, k - 1);
+                    swap(data, k, k - 1);
                     tmpIx = k - 1;
                 }
             }
@@ -118,26 +113,93 @@ public:
         }
     }
 
-    void dataExchange(FieldI& data, int ix, int jx) {
-        int tmp = data[ix];
+    void swap(Field_<T>& data, int ix, int jx) {
+        T tmp = data[ix];
         data[ix] = data[jx];
         data[jx] = tmp;
     }
-
-    FieldI data;
-protected:
-    std::mt19937 gen;
-    std::uniform_int_distribution<int> rnd;
 };
 
+namespace Functions {
+    template<typename T>
+    Field_<T>& dataGeneration(Field_<T>& data) {
+        std::mt19937 gen(std::random_device{} ());
+        std::uniform_int_distribution<T> rnd(-1000, 1000);
+        for (int i = 0; i < data.n; i++)
+                data[i] = rnd(gen);
+        return data;
+    }
+
+    template<typename T>
+    void dataShow(Field_<T>& data) {
+        for (int i = 0; i < data.n; i++)
+            cout << data(i) << ((i + 1) % 15 ? ' ' : '\n');
+    }
+
+    template<typename T>
+    bool isEqual(Field_<T>& arr1, Field_<T>& arr2) {
+        bool equal = true;
+        for(int i = 0; i < arr1.n; i++)
+            if(arr1[i] != arr2[i]) {
+                equal = false;
+                break;
+            }
+        return equal;
+    }
+
+    template<typename T>
+    void testCorrect(sortAlgorithm<T>& solver, int num) {
+        Field_<T> data(num);
+
+        int testTime = 30000;
+        bool succed = true;
+        for(int it = 0; it < testTime; it++) {
+            FieldI arr1 = dataGeneration(data);
+            FieldI arr2 = arr1, arr3 = arr1;
+            // before sort
+            // cout << "Before Sort" << endl;
+            // cout << "【arr1】" << endl;
+            // dataShow(arr1);
+            // cout << endl << "【arr2】" << endl;
+            // dataShow(arr2);
+            // cout << endl << "【arr3】" << endl;
+            // dataShow(arr3);
+            
+            // sort
+            solver.bubbleSort3(arr1);
+            sort(arr2.data, arr2.data + arr2.n);
+            if(!isEqual(arr1, arr2)) {
+                succed = false;
+                break;
+            }
+            
+            // after sort
+            // cout << endl << endl << "After Sort" << endl;
+            // cout << "【arr1】" << endl;
+            // dataShow(arr1);
+            // cout << endl << "【arr2】" << endl;
+            // dataShow(arr2);
+            // cout << endl << "【arr3】" << endl;
+            // dataShow(arr3);
+        }
+        cout << endl << (succed ? "Sort algorithm correct!!" : "So sorry, you failed!!") << endl;
+    }
+};
+
+/*=======================================================*/
 int main(int argc, char* argv[]) {
     int num = 20;
     if(argc > 1)
         num = atoi(argv[1]);
-    Bubble bubble(num);
-    bubble.dataGeneration();
+
+    sortAlgorithm<int> solver;
+    Functions::testCorrect<int>(solver, num);
+
+
+    FieldI data(num);
+    data = Functions::dataGeneration<int>(data);
     cout << "Before sort:" << endl;
-    bubble.dataShow();
+    Functions::dataShow(data);
     
     auto timeS = chrono::high_resolution_clock::now();
     bubble.bubbleSort();
@@ -147,7 +209,7 @@ int main(int argc, char* argv[]) {
     bubble.dataShow();
     cout << "===============================" <<endl;
 
-    bubble.dataGeneration();
+    data = Functions::dataGeneration<int>(data);
     cout << endl << "Before sort:" << endl;
     bubble.dataShow();
     timeS = chrono::high_resolution_clock::now();
@@ -158,7 +220,7 @@ int main(int argc, char* argv[]) {
     bubble.dataShow();
     cout << "===============================" <<endl;
 
-    bubble.dataGeneration();
+    data = Functions::dataGeneration<int>(data);
     cout << endl << "Before sort:" << endl;
     bubble.dataShow();
     timeS = chrono::high_resolution_clock::now();
@@ -169,7 +231,7 @@ int main(int argc, char* argv[]) {
     bubble.dataShow();
     cout << "===============================" <<endl;
 
-    bubble.dataGeneration();
+    data = Functions::dataGeneration<int>(data);
     cout << endl << "Before sort:" << endl;
     bubble.dataShow();
     timeS = chrono::high_resolution_clock::now();
